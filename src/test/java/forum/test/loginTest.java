62f814c3b4eb420f1ddaa15c42d92c3ec4574305
MHayet <marc.hayet@gmail.com>
2015-11-03 17:06:01 +0100

    Renommage de classes

--- src/test/java/forum/test/loginTest.java
+++ src/test/java/forum/test/loginTest.java
@@ -21,7 +21,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes=SpringConfig.class)
-public class loginTest {
+public class LoginTest {
     
     @Autowired
     private UtilisatorService service;
--- src/test/java/forum/test/messagePriveTest.java
+++ src/test/java/forum/test/messagePriveTest.java
@@ -25,7 +25,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes=SpringConfig.class)
-public class messagePriveTest {
+public class MessagePriveTest {
     
     @Autowired
     private UtilisatorService serviceU;
